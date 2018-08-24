f5d9b6f4799e14784aa4fb064cd2ed4636231a48
thaycacac <thaycacac@gmail.com>
2018-08-24 22:04:18 +0700

    upload register.jsp

---
+++ PRJ321/src/java/query/Query.java
@@ -0,0 +1,9 @@
+package query;
+
+/**
+ *
+ * @author Thaycacac
+ */
+public class Query {
+    private static String INSERT_STUDENT = "INSERT INTO Student(firstName, lastName, DOB, gender, address, telephone, email) VALUES (N'?', N'?', ?, ?, N'?', '?', '?')";
+}
